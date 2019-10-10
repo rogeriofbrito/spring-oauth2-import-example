c3fdfeb9e2ab8cca7cbd352fc83ec28358d79a87
Rogério <rogeriobritoufu@gmail.com>
2019-10-10 20:40:25 -0300

    First commit.

---
+++ src/main/java/com/github/rogeriofbrito/springoauth2importexample/SpringOauth2ImportExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.rogeriofbrito.springoauth2importexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2ImportExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauth2ImportExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/rogeriofbrito/springoauth2importexample/SpringOauth2ImportExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.rogeriofbrito.springoauth2importexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringOauth2ImportExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
